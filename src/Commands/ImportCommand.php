4a44506029034a228520f10e2a83c30e74dea368
Dominic Martineau <dominic.martineau@ellipse-synergie.com>
2016-11-22 01:31:05 +0000

    Initialize Import command

--- src/Commands/ImportCommand.php
+++ src/Commands/ImportCommand.php
@@ -3,6 +3,9 @@
 namespace EllipseSynergie\LocaleToCsv\Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 
 /**
  * Class ImportCommand
@@ -10,4 +13,23 @@ use Symfony\Component\Console\Command\Command;
  */
 class ImportCommand extends Command
 {
+    /**
+     */
+    protected function configure()
+    {
+        $this->setName('lang:import-from-csv')
+            ->setDescription('Import locale file to PHP.')
+            ->setHelp("This command allows you to import a lang file from comma-separated values to PHP...")
+            ->addArgument('in', InputArgument::REQUIRED, 'The input filename.')
+            ->addArgument('out', InputArgument::REQUIRED, 'The output filename.');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Not yet implemented.');
+    }
 }
--- tests/Commands/ImportCommandTest.php
+++ tests/Commands/ImportCommandTest.php
@@ -2,6 +2,10 @@
 
 namespace EllipseSynergie\LocaleToCsv\Tests\Commands;
 
+use EllipseSynergie\LocaleToCsv\Commands\ImportCommand;
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Console\Application;
+
 /**
  * Class ImportCommandTest
  *
@@ -10,7 +14,21 @@ namespace EllipseSynergie\LocaleToCsv\Tests\Commands;
  */
 class ImportCommandTest extends \PHPUnit_Framework_TestCase
 {
-    public function testImportSucceed()
+    public function testExportSucceed()
     {
+        $application = new Application();
+        $application->add(new ImportCommand());
+
+        $command = $application->find('lang:import-from-csv');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'command'  => $command->getName(),
+            'in' => 'tests/locale.csv',
+            'out' => 'tests/locale.php',
+        ));
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+        $this->assertContains('Not yet implemented.', $output);
     }
 }
